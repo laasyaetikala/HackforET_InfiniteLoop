c3c4acb780857216cc9cceff1ba06b243ef2d500
Laasya Etikala <laasya.etikala@gmail.com>
2022-04-15 13:35:10 +0530

    changed

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app=Flask(__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='waste_management'
+app.config['MYSQL_DB']='hostel_booking'
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 
 #init MySQL
